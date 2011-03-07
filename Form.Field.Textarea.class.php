19e4317e3b52d162467041b33ca929a64ba9bd72
Shane Smith <cap.webmanager@gmail.com>
2011-03-07 12:15:31 -0500

    Updated Form Textarea

--- Form.Field.Textarea.class.php
+++ Form.Field.Textarea.class.php
@@ -1,50 +1,47 @@
 <?php
+require_once dirname(__FILE__) . "/Form.class.php";
 
-require_once "Form.Field.class.php";
-
-require_once "Form.utils.php";
-
+/**
+*
+* FORM_TEXTAREA
+*
+*
+* An HTML textarea
+*
+*/
 class FORM_TEXTAREA extends FORM_FIELD {
 
+	/**
+	* Type of Form Element
+	*
+	* @var string
+	*/
 	static private $type = 'textarea';
-	protected static $static_renderer, $static_field_renderer;
 
-	public function html() {
-		$this->field_addAttributes(array(
-			'name' => $this->name
-		));
-
-		$value = $this->value();
-		$attributes = $this->field_attr2str();
-
-		return "<textarea {$attributes}>{$value}</textarea>";
-	}
-
-	public function render($renderer=null) {
-		if (is_callable($renderer)) return call_user_func($renderer, $this);
-		elseif (is_callable($this->renderer)) return call_user_func($this->renderer, $this);
-		elseif (is_callable(self::$static_renderer)) return call_user_func(self::$static_renderer, $this);
-		elseif (is_callable(parent::$base_renderer)) return call_user_func(parent::$base_renderer, $this);
-		else return "";
-	}
-
-	public function render_field($field_renderer=null) {
-		if (is_callable($field_renderer)) return call_user_func($field_renderer, $this);
-		elseif (is_callable($this->field_renderer)) return call_user_func($this->field_renderer, $this);
-		elseif (is_callable(self::$static_field_renderer)) return call_user_func(self::$static_field_renderer, $this);
-		elseif (is_callable(parent::$base_field_renderer)) return call_user_func(parent::$base_field_renderer, $this);
-		else return "";
-	}
-
+	/**
+	 * Return the element's type
+	 *
+	 * @return string
+	 */
 	public function type() { return self::$type; }
 
-	public static function renderer($renderer) {
-		if (isset($this)) $this->renderer = $renderer;
-		else self::$static_renderer = $renderer;
-	}
+	/**
+	* Render the textarea field, overwitting basic input tag renderer.
+	*
+	* @return string
+	*/
+	public function render_field() {
+		$attributes = array_merge(
+			$this->getFieldAttributesArray(),
+			array(
+				'type' => $this->type(),
+				'name' => $this->name(),
+			)
+		);
 
-	public static function static_field_renderer($field_renderer) {
-		self::$static_field_renderer = $field_renderer;
+		$attributes = self::attr2str($attributes);
+
+		return "<textarea {$attributes}></textarea>";
 	}
 
 }
