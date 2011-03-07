cc818dd859b76aa2e585a46a9fad24931bd8ffb3
Shane Smith <cap.webmanager@gmail.com>
2011-03-07 12:03:27 -0500

    Updated Form Text

--- Form.Field.Text.class.php
+++ Form.Field.Text.class.php
@@ -1,41 +1,28 @@
 <?php
+require_once dirname(__FILE__) . "/Form.class.php";
 
-require_once "Form.Field.class.php";
-
-require_once "Form.utils.php";
-
+/**
+*
+* FORM_TEXT
+*
+*
+* An HTML textfield
+*
+*/
 class FORM_TEXT extends FORM_FIELD {
 
+	/**
+	* Type of Form Element
+	*
+	* @var string
+	*/
 	static private $type = 'text';
-	protected static $static_renderer, $static_field_renderer;
 
-	public function html() { return parent::html('text'); }
-
-	public function render($renderer=null) {
-		if (is_callable($renderer)) return call_user_func($renderer, $this);
-		elseif (is_callable($this->renderer)) return call_user_func($this->renderer, $this);
-		elseif (is_callable($this->parent()->field_renderer(self::$type))) return call_user_func($this->parent()->field_renderer(self::$type), $this);
-		elseif (is_callable(self::$static_renderer)) return call_user_func(self::$static_renderer, $this);
-		elseif (is_callable(parent::$base_renderer)) return call_user_func(parent::$base_renderer, $this);
-		else return "";
-	}
-
-	public function render_field($field_renderer=null) {
-		if (is_callable($field_renderer)) return call_user_func($field_renderer, $this);
-		elseif (is_callable($this->field_renderer)) return call_user_func($this->field_renderer, $this);
-		elseif (is_callable(self::$static_field_renderer)) return call_user_func(self::$static_field_renderer, $this);
-		elseif (is_callable(parent::$base_field_renderer)) return call_user_func(parent::$base_field_renderer, $this);
-		else return "";
-	}
-
+	/**
+	 * Return the element's type
+	 *
+	 * @return string
+	 */
 	public function type() { return self::$type; }
 
-	public static function renderer($renderer) {
-		if (isset($this)) $this->renderer = $renderer;
-		else self::$static_renderer = $renderer;
-	}
-
-	public static function static_field_renderer($field_renderer) {
-		self::$static_field_renderer = $field_renderer;
-	}
 }
