f2062906fe13ded3438259d16109dd080c5b2094
Shane Smith <cap.webmanager@gmail.com>
2011-03-07 12:19:21 -0500

    Updated Form Hidden

--- Form.Field.Hidden.class.php
+++ Form.Field.Hidden.class.php
@@ -1,47 +1,28 @@
 <?php
+require_once dirname(__FILE__) . "/Form.class.php";
 
-require_once "Form.Field.class.php";
-
-require_once "Form.utils.php";
-
+/**
+*
+* FORM_HIDDEN
+*
+*
+* An HTML hiden field
+*
+*/
 class FORM_HIDDEN extends FORM_FIELD {
 
+	/**
+	* Type of Form Element
+	*
+	* @var string
+	*/
 	static private $type = 'hidden';
-	protected static $static_renderer = array('self', '_default_hidden_renderer'), $static_field_renderer;
 
-	public function __construct($parent, $name, $default=null) {
-		parent::__construct($parent, $name, '', $default);
-	}
-
-	public function html() { return parent::html('hidden'); }
-
-	public function render($renderer=null) {
-		if (is_callable($renderer)) return call_user_func($renderer, $this);
-		elseif (is_callable($this->renderer)) return call_user_func($this->renderer, $this);
-		elseif (is_callable(self::$static_renderer)) return call_user_func(self::$static_renderer, $this);
-		elseif (is_callable(parent::$base_renderer)) return call_user_func(parent::$base_renderer, $this);
-		else return "";
-	}
-
-	public function render_field($field_renderer=null) {
-		if (is_callable($field_renderer)) return call_user_func($field_renderer, $this);
-		elseif (is_callable($this->field_renderer)) return call_user_func($this->field_renderer, $this);
-		elseif (is_callable(self::$static_field_renderer)) return call_user_func(self::$static_field_renderer, $this);
-		elseif (is_callable(parent::$base_field_renderer)) return call_user_func(parent::$base_field_renderer, $this);
-		else return "";
-	}
-
+	/**
+	 * Return the element's type
+	 *
+	 * @return string
+	 */
 	public function type() { return self::$type; }
-
-	public static function renderer($renderer) {
-		if (isset($this)) $this->renderer = $renderer;
-		else self::$static_renderer = $renderer;
-	}
-
-	public static function static_field_renderer($field_renderer) {
-		self::$static_field_renderer = $field_renderer;
-	}
-
-	static function _default_hidden_renderer($element) { return $element->html(); }
 
 }
